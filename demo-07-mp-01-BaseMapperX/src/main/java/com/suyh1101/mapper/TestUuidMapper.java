7d509d243aefa1feeb296b2a834961f10b420b42
suyh <su_787910081@sina.com>
2024-06-22 17:31:12 +0800

    first commit

---
+++ demo-07-mp-01-BaseMapperX/src/main/java/com/suyh1101/mapper/TestUuidMapper.java
@@ -0,0 +1,13 @@
+package com.suyh1101.mapper;
+
+import com.suyh1101.entity.TestUuidEntity;
+import com.suyh1101.mybatis.BaseMapperX;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author suyh
+ * @since 2023-12-09
+ */
+@Mapper
+public interface TestUuidMapper extends BaseMapperX<TestUuidEntity> {
+}
